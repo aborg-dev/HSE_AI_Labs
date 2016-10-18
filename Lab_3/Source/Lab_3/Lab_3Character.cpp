bddb4fd58576a907014cd4b14de7ffa90004368e
akashin <kashin.andrej@gmail.com>
2016-10-18 07:10:26 +0300

    [Lab_3] Fix asset path

--- Lab_3/Source/Lab_3/Lab_3Character.cpp
+++ Lab_3/Source/Lab_3/Lab_3Character.cpp
@@ -38,7 +38,7 @@ ALab_3Character::ALab_3Character()
 	// Create a decal in the world to show the cursor's location
 	CursorToWorld = CreateDefaultSubobject<UDecalComponent>("CursorToWorld");
 	CursorToWorld->SetupAttachment(RootComponent);
-	static ConstructorHelpers::FObjectFinder<UMaterial> DecalMaterialAsset(TEXT("Material'/Game/TopDownCPP/Blueprints/M_Cursor_Decal.M_Cursor_Decal'"));
+	static ConstructorHelpers::FObjectFinder<UMaterial> DecalMaterialAsset(TEXT("Material'/Game/GameContent/TopDownCPP/Blueprints/M_Cursor_Decal.M_Cursor_Decal'"));
 	if (DecalMaterialAsset.Succeeded())
 	{
 		CursorToWorld->SetDecalMaterial(DecalMaterialAsset.Object);
