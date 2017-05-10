6e5cf8c24789db3785a89daa23212041b644d2ae
akashin <kashin.andrej@gmail.com>
2017-05-10 21:28:42 +0300

    Use default tf device selection

--- Lab_4/Content/Scripts/dqn.py
+++ Lab_4/Content/Scripts/dqn.py
@@ -21,43 +21,42 @@ def max_pool_2x2(x):
 
 
 def createNetwork(action_count):
-    with tf.device('/gpu:0'):
-        # network weights
-        W_conv1 = weight_variable([8, 8, 4, 32])
-        b_conv1 = bias_variable([32])
+    # network weights
+    W_conv1 = weight_variable([8, 8, 4, 32])
+    b_conv1 = bias_variable([32])
 
-        W_conv2 = weight_variable([4, 4, 32, 64])
-        b_conv2 = bias_variable([64])
+    W_conv2 = weight_variable([4, 4, 32, 64])
+    b_conv2 = bias_variable([64])
 
-        W_conv3 = weight_variable([3, 3, 64, 64])
-        b_conv3 = bias_variable([64])
+    W_conv3 = weight_variable([3, 3, 64, 64])
+    b_conv3 = bias_variable([64])
 
-        W_fc1 = weight_variable([1600, 512])
-        b_fc1 = bias_variable([512])
+    W_fc1 = weight_variable([1600, 512])
+    b_fc1 = bias_variable([512])
 
-        W_fc2 = weight_variable([512, action_count])
-        b_fc2 = bias_variable([action_count])
+    W_fc2 = weight_variable([512, action_count])
+    b_fc2 = bias_variable([action_count])
 
-        # input layer
-        s = tf.placeholder("float", [None, 80, 80, 4])
+    # input layer
+    s = tf.placeholder("float", [None, 80, 80, 4])
 
-        # hidden layers
-        h_conv1 = tf.nn.relu(conv2d(s, W_conv1, 4) + b_conv1)
-        h_pool1 = max_pool_2x2(h_conv1)
+    # hidden layers
+    h_conv1 = tf.nn.relu(conv2d(s, W_conv1, 4) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
 
-        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2)
-        #h_pool2 = max_pool_2x2(h_conv2)
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2)
+    #h_pool2 = max_pool_2x2(h_conv2)
 
-        h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
-        #h_pool3 = max_pool_2x2(h_conv3)
+    h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
+    #h_pool3 = max_pool_2x2(h_conv3)
 
-        #h_pool3_flat = tf.reshape(h_pool3, [-1, 256])
-        h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
+    #h_pool3_flat = tf.reshape(h_pool3, [-1, 256])
+    h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
 
-        h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
+    h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
 
-        # readout layer
-        readout = tf.matmul(h_fc1, W_fc2) + b_fc2
+    # readout layer
+    readout = tf.matmul(h_fc1, W_fc2) + b_fc2
 
     return s, readout, h_fc1
 
